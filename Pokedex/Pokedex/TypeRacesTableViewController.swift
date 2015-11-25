b0707bd0b8f7139382ce4d3a9082877004334484
carlosv5 <carlos.vegagarcia@hotmail.com>
2015-11-25 18:34:43 +0100

    Version acabada

---
+++ Pokedex/Pokedex/TypeRacesTableViewController.swift
@@ -0,0 +1,125 @@
+//
+//  TypeRacesTableViewController.swift
+//  Pokedex
+//
+//  Created by Carlos on 24/11/15.
+//  Copyright © 2015 UPM. All rights reserved.
+//
+
+import UIKit
+
+class TypeRacesTableViewController: UITableViewController {
+
+    
+    var type: Type?
+
+    
+    override func viewWillAppear(animated: Bool) {
+        self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
+        super.viewWillAppear(animated)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+            return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if type != nil {
+            return type!.races.count        }
+        else{
+            return 1
+        }    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Race Cell", forIndexPath: indexPath)
+        if type != nil {
+            let race = type!.races[indexPath.row]
+    
+        cell.imageView?.image = UIImage(named: race.icon)
+        cell.textLabel?.text = race.name
+        cell.detailTextLabel?.text = race.code
+        }
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if(type != nil){
+        return String(type!)
+        }
+        else{
+            return "unknown"
+        }
+    }
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return false if you do not want the specified item to be editable.
+    return true
+    }
+    */
+    
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    if editingStyle == .Delete {
+    // Delete the row from the data source
+    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
+    }
+    */
+    
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+    
+    }
+    */
+    
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return false if you do not want the item to be re-orderable.
+    return true
+    }
+    */
+    
+    // MARK: - Navigation
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if segue.identifier == "Show Web Info" {
+            // El destino del segue es el Navigation Controller.
+            // El primer VC apuntado por el Navigation Controller el WebVC.
+            // sender es la celda de la tabla que disparo el segue.
+            if let nc = segue.destinationViewController as? UINavigationController,
+                let wvc = nc.topViewController as? WebViewController,
+                let cell = sender as? UITableViewCell,
+                let ip = tableView.indexPathForCell(cell) {
+                    wvc.race = type!.races[ip.row]
+                    wvc.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+                    wvc.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+}
---
+++ Pokedex/Pokedex/TypeTableViewController.swift
@@ -0,0 +1,115 @@
+//
+//  RacesTableViewController.swift
+//  Pokedex
+//
+//  Created by Carlos on 24/11/15.
+//  Copyright © 2015 UPM. All rights reserved.
+//
+
+import UIKit
+
+class TypeTableViewController: UITableViewController {
+    
+    var pokedexModel = PokedexModel()
+    
+    override func viewWillAppear(animated: Bool) {
+        self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
+        super.viewWillAppear(animated)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let type = pokedexModel.types
+        return type.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Type Cell", forIndexPath: indexPath)
+        
+        let type = pokedexModel.types[indexPath.row]
+        
+        cell.imageView?.image = UIImage(named: type.icon)
+        cell.textLabel?.text = type.name
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Razas"
+    }
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return false if you do not want the specified item to be editable.
+    return true
+    }
+    */
+    
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    if editingStyle == .Delete {
+    // Delete the row from the data source
+    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
+    }
+    */
+    
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+    
+    }
+    */
+    
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return false if you do not want the item to be re-orderable.
+    return true
+    }
+    */
+    
+    // MARK: - Navigation
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if segue.identifier == "Show Races" {
+            // El destino del segue es el Navigation Controller.
+            // El primer VC apuntado por el Navigation Controller el WebVC.
+            // sender es la celda de la tabla que disparo el segue.
+            if let nc = segue.destinationViewController as? TypeRacesTableViewController,
+                let cell = sender as? UITableViewCell,
+                let ip = tableView.indexPathForCell(cell) {
+                    nc.type = pokedexModel.types[ip.row]
+                    nc.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+                    nc.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+    
+}
